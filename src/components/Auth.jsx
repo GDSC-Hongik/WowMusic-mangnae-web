8ac45b49c9e984c01562194ac3226abe563f0a0f
tablemin03 <vs201466@naver.com>
2025-02-02 13:35:35 +0900

    feat: api 주소 변경

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,16 +2,16 @@ import axios from 'axios'
 
 export const login = async ({email, password}) =>{
     try {
-        const res = await axios.post("http://127.0.0.1:8000/users/login", {
+        const res = await axios.post("http://192.168.212.15:8000/api/users/login/", {
             email,
             password,
         });
         localStorage.setItem("token", res.data.token);
         console.log("토큰 발급 완료");
-        return res.data; // 로그인 결과 반환
+        return res.data;
     }
     catch (error) {
         console.error("로그인 실패:", error);
-        throw error; // 에러 발생 시 throw
+        throw error;
     }
 };
