c0d157d05de9e7a77a5b0dbf650a46f2b5f4eb6e
tablemin03 <vs201466@naver.com>
2025-02-05 10:39:17 +0900

    ListPage 디자인, LoginPage 디자인 수정

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import style from "../components/ListPage.module.css"
 
 const ListPage = () =>{
+    const nav = useNavigate();
     return(
         <div className={style.page}>
-        <br />
-        <br />
-        <br />
-        <button><Link to="/keyword">KEYWORD</Link></button>
-        <button><Link to="/fortune">FORTUNE</Link></button>
-        <button><Link to="/mbti">MBTI</Link></button>
+            <ul className={style.ul}>
+                <li className={style.list} onClick={()=> nav("/keyword")}>KEYWORD</li>
+                <hr />
+                <li className={style.list} onClick={()=> nav("/fortune")}>FORTUNE</li>
+                <hr />
+                <li className={style.list} onClick={()=> nav("/mbti")}>MBTI</li>
+            </ul>
     </div>
     )
 }
